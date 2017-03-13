047e29d7a435438cd41d957c1aced16c7316f941
Mikhail Mandronov <mikhail.mandronov@trasd.com>
2017-03-13 14:46:31 +0600

    update annotation

--- spec/models/movie_spec.rb
+++ spec/models/movie_spec.rb
@@ -1,3 +1,19 @@
+# == Schema Information
+#
+# Table name: movies
+#
+#  id           :integer          not null, primary key
+#  title        :string           not null
+#  description  :text             not null
+#  user_id      :integer          not null
+#  is_deleted   :boolean          default("false")
+#  created_at   :datetime         not null
+#  updated_at   :datetime         not null
+#  total_score  :integer          default("0")
+#  avg_score    :float            default("0.0")
+#  votes_number :integer          default("0")
+#
+
 require 'rails_helper'
 
 RSpec.describe Movie, type: :model do
--- test/models/movie_test.rb
+++ test/models/movie_test.rb
@@ -5,7 +5,7 @@
 #  id           :integer          not null, primary key
 #  title        :string           not null
 #  description  :text             not null
-#  user_id      :integer
+#  user_id      :integer          not null
 #  is_deleted   :boolean          default("false")
 #  created_at   :datetime         not null
 #  updated_at   :datetime         not null
